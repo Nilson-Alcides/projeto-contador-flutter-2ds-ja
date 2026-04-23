0eb346708b178ce51d82ab443d93a5107b64bead
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-04-23 19:56:48 -0300

    add projeto

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MyApp()
+  );
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext content){
+    return MaterialApp(     
+      home: HomePage(), 
+  );
+  }
+}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void decrement(){print('decrement');}
+  void increment(){print('decrement');}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(         
+          image: DecorationImage(
+            image: AssetImage('assets/images/image.jpg'),
+            fit: BoxFit.cover,
+            ),
+        ),
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Pode entrar',
+          style: TextStyle(
+            fontSize: 26,
+            color:  Color.fromARGB(255, 4, 87, 154),
+            fontWeight: FontWeight.w700
+          ),
+          ),
+        
+          Padding(padding: EdgeInsets.all(24),
+          child: Text('0',
+           style: TextStyle(
+            fontSize: 26,
+            color:  Color.fromARGB(255, 4, 87, 154),
+            fontWeight: FontWeight.w700
+          ),
+          ),
+          ),
+           
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(onPressed: decrement,
+              style: TextButton.styleFrom(
+                  backgroundColor:  Colors.blue,
+                  fixedSize: const Size(100, 100),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),),
+               child: Text('Saiu'),
+              ),
+
+              SizedBox(width: 32,),
+
+              TextButton(onPressed: increment,
+              style: TextButton.styleFrom(
+                  backgroundColor:  Colors.blue,
+                  fixedSize: const Size(100, 100),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),),
+               child: Text('Entrar'),
+              ),
+            ],
+          ),
+        ],
+      ),
+      )
+    );
+  }
+}
