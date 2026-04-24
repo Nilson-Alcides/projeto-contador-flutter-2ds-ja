e981c58d3f3b27ad8a6b872519b0480bb7f338a5
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-04-23 22:32:42 -0300

    Adicionando funcionalidade no botão incremento e decremento + mesg Debug off

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,38 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext content){
-    return MaterialApp(     
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false,    
       home: HomePage(), 
   );
   }
 }
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
 
-  void decrement(){print('decrement');}
-  void increment(){print('decrement');}
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int contador = 0;
+
+  void decrement(){
+    setState(() {
+      contador--;
+    });       
+      print(contador);
+     }
+
+   void increment(){
+    setState(() {
+      contador++;
+    });     
+      print(contador);
+     }
+
+    bool get isEmpty => contador == 0;
+    bool get isFull => contador == 10;
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +65,7 @@ class HomePage extends StatelessWidget {
           ),
         
           Padding(padding: EdgeInsets.all(24),
-          child: Text('0',
+          child: Text(contador.toString(),
            style: TextStyle(
             fontSize: 26,
             color:  Color.fromARGB(255, 4, 87, 154),
@@ -55,9 +77,10 @@ class HomePage extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextButton(onPressed: decrement,
+              TextButton(
+                onPressed: isEmpty ? null : decrement,
               style: TextButton.styleFrom(
-                  backgroundColor:  Colors.blue,
+                  backgroundColor:isEmpty ? Colors.blue.withOpacity(0.2) : Colors.blue,
                   fixedSize: const Size(100, 100),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(24),
@@ -67,9 +90,10 @@ class HomePage extends StatelessWidget {
 
               SizedBox(width: 32,),
 
-              TextButton(onPressed: increment,
-              style: TextButton.styleFrom(
-                  backgroundColor:  Colors.blue,
+              TextButton(
+                onPressed: isFull ? null : increment,                 
+                style: TextButton.styleFrom(
+                  backgroundColor: isFull ? Colors.blue.withOpacity(0.2) : Colors.blue,
                   fixedSize: const Size(100, 100),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(24),
